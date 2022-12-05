f7aa4f05c90b56ea6d5034fabc848a50cf16201e
Karim Mohamed <komicho1996@gmail.com>
2022-12-05 23:30:51 +0200

    Feat: create the `Customer` model

---
+++ src/Models/Customer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OutMart\Laravel\Customers\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'outmart_customers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'metadata',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'metadata' => 'json',
+    ];
+
+    public function customerable()
+    {
+        return $this->morphTo();
+    }
+}
