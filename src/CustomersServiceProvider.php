02aeebe5eac729ceac1b3b7d69e9263aae36491b
Karim Mohamed <komicho1996@gmail.com>
2022-12-05 22:17:06 +0200

    Feat: create `ServiceProvider` for this package

---
+++ src/CustomersServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OutMart\Laravel\Customers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomersServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
