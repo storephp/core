009600ca4f97d06d76abc2edbd4807191b8e9ed0
Karim Mohamed <komicho1996@gmail.com>
2022-12-28 19:14:32 +0200

    Remove customer and basket facades

--- src/OutMartServiceProvider.php
+++ src/OutMartServiceProvider.php
@@ -3,8 +3,6 @@
 namespace OutMart;
 
 use Illuminate\Support\ServiceProvider;
-use OutMart\Manage\Baskets\BasketManager;
-use OutMart\Manage\Customers\CustomerManager;
 
 class OutMartServiceProvider extends ServiceProvider
 {
@@ -20,14 +18,6 @@ class OutMartServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/baskets.php', 'outmart.baskets');
 
         $this->mergeConfigFrom(__DIR__ . '/../config/customers.php', 'outmart.customers');
-
-        $this->app->singleton('customer', function () {
-            return new CustomerManager();
-        });
-
-        $this->app->singleton('basket', function () {
-            return new BasketManager();
-        });
     }
 
     /**
