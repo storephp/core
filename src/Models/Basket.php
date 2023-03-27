92bedeb0fb38ebf295bc323453ad9db0eb6a3bf4
Karim Mohamed <komicho1996@gmail.com>
2023-03-27 04:12:49 +0200

    Feat: optimize basket service

--- src/Models/Basket.php
+++ src/Models/Basket.php
@@ -2,21 +2,14 @@
 
 namespace OutMart\Models;
 
-use Exception;
 use Illuminate\Support\Str;
 use OutMart\Base\ModelBase;
-use OutMart\Contracts\ICondition;
-use OutMart\Contracts\Model\IFinalPrice;
-use OutMart\DataType\ProductSku;
-use OutMart\Enums\Baskets\Status;
 use OutMart\Events\Basket\BasketCreated;
 use OutMart\Events\Basket\BasketCreating;
 use OutMart\Events\Basket\BasketUpdated;
 use OutMart\Events\Basket\BasketUpdating;
 use OutMart\Models\Basket\Coupon;
 use OutMart\Models\Basket\Quote;
-use OutMart\Models\Order\Address;
-use OutMart\PricingRules\Lay;
 
 class Basket extends ModelBase
 {
@@ -91,25 +84,25 @@ class Basket extends ModelBase
     //     return $quote;
     // }
 
-    public function applyCoupon($coupon)
-    {
-        if ($coupon = Coupon::where('coupon_code', $coupon)->first()) {
-            $this->coupon_code = $coupon->coupon_code;
-            $this->save();
-        }
+    // public function applyCoupon($coupon)
+    // {
+    //     if ($coupon = Coupon::where('coupon_code', $coupon)->first()) {
+    //         $this->coupon_code = $coupon->coupon_code;
+    //         $this->save();
+    //     }
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
-    public function resetCoupon()
-    {
-        if ($this->coupon_code) {
-            $this->coupon_code = null;
-            $this->save();
-        }
+    // public function resetCoupon()
+    // {
+    //     if ($this->coupon_code) {
+    //         $this->coupon_code = null;
+    //         $this->save();
+    //     }
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
     public function coupon()
     {
@@ -136,185 +129,185 @@ class Basket extends ModelBase
         return $this->morphTo();
     }
 
-    public function canUpdateStatus()
-    {
-        return in_array($this->status, [Status::OPENED(), Status::ABANDONED()]);
-    }
+    // public function canUpdateStatus()
+    // {
+    //     return in_array($this->status, [Status::OPENED(), Status::ABANDONED()]);
+    // }
 
-    public function canPlaceOrder()
-    {
-        return $this->quotes()->exists() && in_array($this->status, [Status::OPENED(), Status::ABANDONED()]);
-    }
+    // public function canPlaceOrder()
+    // {
+    //     return $this->quotes()->exists() && in_array($this->status, [Status::OPENED(), Status::ABANDONED()]);
+    // }
 
-    public function getShippingMethod()
-    {
-        return $this->shippingMethod;
-    }
+    // public function getShippingMethod()
+    // {
+    //     return $this->shippingMethod;
+    // }
 
-    public function setShippingMethod($shippingMethod)
-    {
-        $this->shippingMethod = $shippingMethod;
+    // public function setShippingMethod($shippingMethod)
+    // {
+    //     $this->shippingMethod = $shippingMethod;
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
-    public function getPaymentMethod()
-    {
-        return $this->paymentMethod;
-    }
+    // public function getPaymentMethod()
+    // {
+    //     return $this->paymentMethod;
+    // }
 
-    public function setPaymentMethod($paymentMethod)
-    {
-        $this->paymentMethod = $paymentMethod;
+    // public function setPaymentMethod($paymentMethod)
+    // {
+    //     $this->paymentMethod = $paymentMethod;
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
-    public function getDiscountTotal(): float
-    {
-        return $this->getTotal() - $this->getSubTotal();
-    }
+    // public function getDiscountTotal(): float
+    // {
+    //     return $this->getTotal() - $this->getSubTotal();
+    // }
 
-    public function getSubTotal(): float
-    {
-        $quotes = $this->quotes()->with('product')->get();
+    // public function getSubTotal(): float
+    // {
+    //     $quotes = $this->quotes()->with('product')->get();
 
-        $total = 0;
+    //     $total = 0;
 
-        if ($quotes) {
-            foreach ($quotes as $quote) {
-                if (!$quote->product instanceof IFinalPrice) {
-                    throw new Exception("You must implement `\OutMart\Contracts\Model\IFinalPrice`");
-                }
+    //     if ($quotes) {
+    //         foreach ($quotes as $quote) {
+    //             if (!$quote->product instanceof IFinalPrice) {
+    //                 throw new Exception("You must implement `\OutMart\Contracts\Model\IFinalPrice`");
+    //             }
 
-                $total += $quote->quantity * $quote->product?->final_price;
-            }
-        }
+    //             $total += $quote->quantity * $quote->product?->final_price;
+    //         }
+    //     }
 
-        return $total;
-    }
+    //     return $total;
+    // }
 
-    public function getTotal(): float
-    {
-        if (!$getSubTotal = $this->getSubTotal())
-            return 0;
+    // public function getTotal(): float
+    // {
+    //     if (!$getSubTotal = $this->getSubTotal())
+    //         return 0;
 
-        $lay = new Lay;
+    //     $lay = new Lay;
 
-        $lay->setTotal($getSubTotal);
+    //     $lay->setTotal($getSubTotal);
 
-        if ($getShippingMethod = $this->getShippingMethod())
-            $lay->setShippingMethod($getShippingMethod);
+    //     if ($getShippingMethod = $this->getShippingMethod())
+    //         $lay->setShippingMethod($getShippingMethod);
 
-        if ($getPaymentMethod = $this->getPaymentMethod())
-            $lay->setPaymentMethod($getPaymentMethod);
+    //     if ($getPaymentMethod = $this->getPaymentMethod())
+    //         $lay->setPaymentMethod($getPaymentMethod);
 
-        // Handle coupon
-        if (($coupon = $this->coupon) && (!$this->coupon->expired))
-            $lay->rule(function ($attributes) use ($coupon) {
+    //     // Handle coupon
+    //     if (($coupon = $this->coupon) && (!$this->coupon->expired))
+    //         $lay->rule(function ($attributes) use ($coupon) {
 
-                // 
-                if ($coupon->condition) {
-                    $conditionObj = config('outmart.coupons.conditions.' . $coupon->condition, null);
-                    if ($conditionObj) {
-                        $condition = new $conditionObj(
-                            $attributes['total'],
-                            $this->quotes()->pluck('product_sku')->toArray(),
-                            $coupon->condition_data,
-                        );
+    //             //
+    //             if ($coupon->condition) {
+    //                 $conditionObj = config('outmart.coupons.conditions.' . $coupon->condition, null);
+    //                 if ($conditionObj) {
+    //                     $condition = new $conditionObj(
+    //                         $attributes['total'],
+    //                         $this->quotes()->pluck('product_sku')->toArray(),
+    //                         $coupon->condition_data,
+    //                     );
 
-                        if (!$condition instanceof ICondition) {
-                            throw new Exception('You need to implement `ICondition` in condition class');
-                        }
+    //                     if (!$condition instanceof ICondition) {
+    //                         throw new Exception('You need to implement `ICondition` in condition class');
+    //                     }
 
-                        return $condition->handle();
-                    }
-                    return false;
-                }
+    //                     return $condition->handle();
+    //                 }
+    //                 return false;
+    //             }
 
-                return true;
-            }, function ($operations) use ($coupon) {
-                $total = $operations->getTotal();
+    //             return true;
+    //         }, function ($operations) use ($coupon) {
+    //             $total = $operations->getTotal();
 
-                if ($coupon->discount_type == 'percentage') {
-                    $discount = ($total * $coupon->discount_value) / 100;
-                    $total = $total - $discount;
-                }
+    //             if ($coupon->discount_type == 'percentage') {
+    //                 $discount = ($total * $coupon->discount_value) / 100;
+    //                 $total = $total - $discount;
+    //             }
 
-                if ($coupon->discount_type == 'fixed')
-                    $total = $total - $coupon->discount_value;
+    //             if ($coupon->discount_type == 'fixed')
+    //                 $total = $total - $coupon->discount_value;
 
-                $operations->setTotal($total);
-            });
+    //             $operations->setTotal($total);
+    //         });
 
-        return $lay->getTotal();
-    }
+    //     return $lay->getTotal();
+    // }
 
-    public function placeOrder($address_id)
-    {
-        if (!$this->canPlaceOrder()) {
-            throw new Exception("You cannot place an order from this basket");
-        }
+    // public function placeOrder($address_id)
+    // {
+    //     if (!$this->canPlaceOrder()) {
+    //         throw new Exception("You cannot place an order from this basket");
+    //     }
 
-        $orderData = [];
-        
-        $orderData['customer_id'] = $this->customer->id;
-        $orderData['status_id'] = 1;
+    //     $orderData = [];
 
-        if (($coupon = $this->coupon) && (!$this->coupon->expired)) {
-            $orderData['discount_details'] = [
-                'discount_type' => 'coupon',
-                'coupon' => [
-                    'coupon_code' => $coupon->coupon_code,
-                    'discount_value' => $coupon->discount_value,
-                    'discount_value' => $coupon->discount_value,
-                    'condition' => $coupon->condition,
-                    'condition_data' => $coupon->condition_data,
-                    'start_at' => $coupon->start_at,
-                    'ends_at' => $coupon->ends_at,
-                ],
-            ];
+    //     $orderData['customer_id'] = $this->customer->id;
+    //     // $orderData['status_id'] = 1;
 
-            $orderData['discount_total'] = $this->getDiscountTotal();
-        }
+    //     if (($coupon = $this->coupon) && (!$this->coupon->expired)) {
+    //         $orderData['discount_details'] = [
+    //             'discount_type' => 'coupon',
+    //             'coupon' => [
+    //                 'coupon_code' => $coupon->coupon_code,
+    //                 'discount_value' => $coupon->discount_value,
+    //                 'discount_value' => $coupon->discount_value,
+    //                 'condition' => $coupon->condition,
+    //                 'condition_data' => $coupon->condition_data,
+    //                 'start_at' => $coupon->start_at,
+    //                 'ends_at' => $coupon->ends_at,
+    //             ],
+    //         ];
 
-        $orderData['sub_total'] = $this->getSubTotal();
-        $orderData['shipping_total'] = 0;
-        $orderData['tax_total'] = 0;
-        $orderData['grand_total'] = $this->getTotal();
+    //         $orderData['discount_total'] = $this->getDiscountTotal();
+    //     }
 
-        if ($this->order()->exists()) {
-            throw new Exception("Already there is an order for this basket");
-        }
+    //     $orderData['sub_total'] = $this->getSubTotal();
+    //     $orderData['shipping_total'] = 0;
+    //     $orderData['tax_total'] = 0;
+    //     $orderData['grand_total'] = $this->getTotal();
 
-        $address = $this->customer->addresses()->find($address_id);
+    //     if ($this->order()->exists()) {
+    //         throw new Exception("Already there is an order for this basket");
+    //     }
 
-        if (!$address) {
-            throw new Exception("Error Processing Request");
-        }
+    //     $address = $this->customer->addresses()->find($address_id);
 
-        $order = $this->order()->create($orderData);
+    //     if (!$address) {
+    //         throw new Exception("Error Processing Request");
+    //     }
 
-        if ($this->canUpdateStatus()) {
-            $this->status = Status::ORDERED();
-            $this->save();
-        }
+    //     $order = $this->order()->create($orderData);
 
-        Address::create([
-            'order_id' => $order->id,
-            'title' => $address->label,
-            'first_name' => $this->customer->first_name,
-            'last_name' => $this->customer->last_name,
-            'street_line_1' => $address->street_line_1,
-            'street_line_2' => $address->street_line_2,
-            'country' => null,
-            'city' => null,
-            'state' => null,
-            'postcode' => $address->postcode,
-            'contact_email' => $this->customer->email,
-            'contact_phone' => $address->telephone_number,
-        ]);
+    //     if ($this->canUpdateStatus()) {
+    //         $this->status = Status::ORDERED();
+    //         $this->save();
+    //     }
 
-        return $order;
-    }
+    //     Address::create([
+    //         'order_id' => $order->id,
+    //         'title' => $address->label,
+    //         'first_name' => $this->customer->first_name,
+    //         'last_name' => $this->customer->last_name,
+    //         'street_line_1' => $address->street_line_1,
+    //         'street_line_2' => $address->street_line_2,
+    //         'country' => null,
+    //         'city' => null,
+    //         'state' => null,
+    //         'postcode' => $address->postcode,
+    //         'contact_email' => $this->customer->email,
+    //         'contact_phone' => $address->telephone_number,
+    //     ]);
+
+    //     return $order;
+    // }
 }
--- src/Services/BasketService.php
+++ src/Services/BasketService.php
@@ -2,40 +2,174 @@
 
 namespace OutMart\Services;
 
+use Exception;
+use OutMart\Contracts\Model\IFinalPrice;
+use OutMart\Enums\Baskets\Status;
 use OutMart\Exceptions\Baskets\QuoteExceedingLimitException;
 use OutMart\Exceptions\Baskets\QuoteTheMaxException;
+use OutMart\Models\Basket\Coupon;
 use OutMart\Models\Basket\Quote;
 use OutMart\Repositories\BasketRepository;
 use OutMart\Repositories\QuoteRepository;
 
 class BasketService
 {
-    public $currentBasket = null;
+    private $currentBasket = null;
+    private $currency = null;
+    private $shippingMethod = null;
+    private $paymentMethod = null;
+    private $shippingTotal = null;
+    private $taxTotal = null;
+    private $discountTotal = null;
 
     public function __construct(
-        public BasketRepository $basketRepository,
-        public QuoteRepository $quoteRepository,
+        private BasketRepository $basketRepository,
+        private QuoteRepository $quoteRepository,
     ) {}
 
-    public function initBasket($ulid = null)
+    /**
+     * Basket init for enabled the use
+     *
+     * @param ulid $ulid
+     * @param string $currency
+     *
+     * @return \OutMart\Services\BasketService
+     */
+    public function initBasket($ulid = null, string $currency = 'EGP')
     {
         $basket = $this->basketRepository->getAvailableBasket($ulid);
 
         // dd($ulid, $basket);
 
         if (!$basket) {
-            $basket = $this->basketRepository->createNewBasket('EGP');
+            $basket = $this->basketRepository->createNewBasket($currency);
         }
 
         $this->currentBasket = $basket;
+        $this->setCurrency($basket->currency);
         $this->quoteRepository->setBasket($basket);
 
         return $this;
     }
 
+    /**
+     * Get the value of currency
+     */
     public function getCurrency()
     {
-        return $this->currentBasket->currency;
+        return $this->currency;
+    }
+
+    /**
+     * Set the value of currency
+     *
+     * @return  self
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of shippingMethod
+     */
+    public function getShippingMethod()
+    {
+        return $this->shippingMethod;
+    }
+
+    /**
+     * Set the value of shippingMethod
+     *
+     * @return  self
+     */
+    public function setShippingMethod($shippingMethod)
+    {
+        $this->shippingMethod = $shippingMethod;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of paymentMethod
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * Set the value of paymentMethod
+     *
+     * @return  self
+     */
+    public function setPaymentMethod($paymentMethod)
+    {
+        $this->paymentMethod = $paymentMethod;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of shippingTotal
+     */
+    public function getShippingTotal()
+    {
+        return $this->shippingTotal;
+    }
+
+    /**
+     * Set the value of shippingTotal
+     *
+     * @return  self
+     */
+    public function setShippingTotal($shippingTotal)
+    {
+        $this->shippingTotal = $shippingTotal;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of taxTotal
+     */
+    public function getTaxTotal()
+    {
+        return $this->taxTotal;
+    }
+
+    /**
+     * Set the value of taxTotal
+     *
+     * @return  self
+     */
+    public function setTaxTotal($taxTotal)
+    {
+        $this->taxTotal = $taxTotal;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of discountTotal
+     */
+    public function getDiscountTotal()
+    {
+        return $this->discountTotal;
+    }
+
+    /**
+     * Set the value of discountTotal
+     *
+     * @return  self
+     */
+    public function setDiscountTotal($discountTotal)
+    {
+        $this->discountTotal = $discountTotal;
+
+        return $this;
     }
 
     public function getUlid()
@@ -56,6 +190,11 @@ class BasketService
         }
 
         return $this->quoteRepository->increase($quote, $quantity);
+    }
+
+    public function getQuotes()
+    {
+        return $this->quoteRepository->getList();
     }
 
     public function increase(Quote $quote, int $quantity)
@@ -79,5 +218,85 @@ class BasketService
         }
 
         return $this->quoteRepository->decrease($quote, $quantity);
+    }
+
+    public function deleteQuote(Quote $quote)
+    {
+        $this->quoteRepository->delete($quote);
+    }
+
+    public function applyCoupon($coupon)
+    {
+        if ($coupon = Coupon::where('coupon_code', $coupon)->first()) {
+            $this->currentBasket->coupon_code = $coupon->coupon_code;
+            $this->currentBasket->save();
+        }
+    }
+
+    public function resetCoupon()
+    {
+        if ($this->currentBasket->coupon_code) {
+            $this->currentBasket->coupon_code = null;
+            $this->currentBasket->save();
+        }
+    }
+
+    public function getCoupon()
+    {
+        return $this->currentBasket->coupon;
+    }
+
+    public function getSubTotal()
+    {
+        $quotes = $this->currentBasket->quotes()->with('product')->get();
+
+        $total = 0;
+
+        if ($quotes) {
+            foreach ($quotes as $quote) {
+                if (!$quote->product instanceof IFinalPrice) {
+                    throw new Exception("You must implement `\OutMart\Contracts\Model\IFinalPrice`");
+                }
+
+                $total += $quote->quantity * $quote->product?->final_price;
+            }
+        }
+
+        return $total;
+    }
+
+    public function getTotal(): float
+    {
+        $discountTotal = ($this->getSubTotal() - $this->getDiscountTotal());
+        $feedsTotal = ($this->getShippingTotal() + $this->getShippingTotal());
+        return ($discountTotal + $feedsTotal);
+    }
+
+    public function canUpdateStatus()
+    {
+        return in_array($this->currentBasket->status, [Status::OPENED(), Status::ABANDONED()]);
+    }
+
+    public function canPlaceOrder()
+    {
+        return $this->quoteRepository->basketHasQuote() && in_array($this->currentBasket->status, [Status::OPENED(), Status::ABANDONED()]);
+    }
+
+    public function prefaceOrder($key = null)
+    {
+        $data = [
+            'basket' => $this->currentBasket,
+            'sub_total' => $this->getSubTotal(),
+            'discount_total' => $this->getDiscountTotal(),
+            'shipping_total' => $this->getShippingTotal(),
+            'tax_total' => $this->getTaxTotal(),
+            'grand_total' => $this->getTotal(),
+        ];
+
+        if ($key && isset($data[$key])) {
+            return $data[$key];
+        }
+
+        return $data;
     }
 }
